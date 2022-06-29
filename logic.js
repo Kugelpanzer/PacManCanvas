17e25d080a0264ce645e45e04eeef445232af9bd
Djordje Mladenovic <djolemlad@gmail.com>
2022-06-29 20:52:30 +0200

    initial push

---
+++ logic.js
@@ -0,0 +1,17 @@
+const canvas = document.getElementById("main-canvas");
+const ctx = document.getElementById('2d');
+
+canvas.width = 800 
+canvas.height = 640
+
+class WallObject{
+    constructor(x,y){
+        
+    }
+
+}
+class MovableObject{
+    constructor(sprite,x,y){
+
+    }
+}
